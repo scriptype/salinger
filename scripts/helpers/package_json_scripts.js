35c2113564beed531c87fd7013f55582213d1850
scriptype <scriptyper@gmail.com>
2017-02-07 22:54:12 +0300

    Fix CI script helper

--- scripts/helpers/package_json_scripts.js
+++ scripts/helpers/package_json_scripts.js
@@ -3,20 +3,19 @@ var fs = require('fs')
 
 var [
   packageJSON,
-  ...scriptsArguments
+  ...scripts
 ] = process.argv.slice(2)
 
 var packageJSONPath = path.join(process.cwd(), packageJSON)
 var pJSON = require(packageJSONPath)
 
-var scripts = scriptsArguments.split(' ')
 var scriptKeys = scripts.filter((s, i) => i % 2 === 0)
 var scriptValues = scripts.filter((s, i) => i % 2)
 
 pJSON.scripts = scriptKeys.reduce((prev, curr, index) => {
   return Object.assign({}, prev, {
-    curr: scriptValues[index]
+    [curr]: scriptValues[index]
   })
 }, {})
 
-fs.writeFileSync(packageJSONPath, JSON.stringify(pJSON))
+fs.writeFileSync(packageJSONPath, JSON.stringify(pJSON, null, 2))
