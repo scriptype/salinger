9c65f92dcb35de3b3db426b30aec6a84d48ca311
scriptype <scriptyper@gmail.com>
2017-02-08 19:39:57 +0300

    [travis] Don't clone the repo itself again

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,14 +1,12 @@
-git clone https://github.com/scriptype/salinger-npm.git salinger
-
 mkdir salinger-test
 cd salinger-test
 
 npm init -y
 npm i -D salinger
 
-cp -R ../salinger/scripts/files/run .
+cp -R ../scripts/files/run .
 
-node ../salinger/scripts/helpers/package_json_scripts \
+node ../scripts/helpers/package_json_scripts \
   package.json \
   start "salinger start" \
   lorem "salinger lorem"
