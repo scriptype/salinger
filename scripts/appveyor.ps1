dad8a567059b0d72e81b606ad73282794541ca51
scriptype <scriptyper@gmail.com>
2017-02-08 20:04:35 +0300

    [appveyor] Fix syntax error in powershell file

--- scripts/appveyor.ps1
+++ scripts/appveyor.ps1
@@ -6,9 +6,9 @@ npm i -D salinger
 
 cp ..\scripts\files\run . -recurse
 
-node ..\scripts\helpers\package_json_scripts \
-  package.json \
-  start "salinger start" \
+node ..\scripts\helpers\package_json_scripts `
+  package.json `
+  start "salinger start" `
   lorem "salinger lorem"
 
 npm start
