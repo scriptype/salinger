647535944d4bc2ea0b8a267d06107dd9886d90f1
scriptype <scriptyper@gmail.com>
2017-02-08 20:25:32 +0300

    [codeclimate] Fix code style issues

--- bin/index.js
+++ bin/index.js
@@ -11,6 +11,5 @@ var [ task, ...restArguments ] = process.argv.slice(2)
 if (task in tasks) {
   tasks[task](...restArguments)
 } else {
-  console.error(`The task ${task} doesn't exist.`)
-  process.exit(1)
+  throw new Error(`The task ${task} doesn't exist.`)
 }
