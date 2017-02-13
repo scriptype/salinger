c797f8dda33836cb219be97905922363045bbd3e
scriptype <scriptyper@gmail.com>
2017-02-14 01:00:13 +0300

    [appveyor] Fix echo syntax

--- scripts/appveyor.ps1
+++ scripts/appveyor.ps1
@@ -9,9 +9,9 @@ node ..\scripts\helpers\packageJsonScripts `
   package.json `
   hello "node ..\bin\index.js hello"
 
-echo "module.exports = {MY_VAR:'my-var'}" > scripts/env.js
-echo "var run=require('../..').run;exports.hello=()=>run('hello')" > scripts/tasks.js
-echo "if(process.env['MY_VAR']!=='my-var')throw 'not ok' " > scripts/tasks/hello.js
+echo "module.exports = {MY_VAR:'my-var'}" | out-file -encoding ASCII scripts/env.js
+echo "var run=require('../..').run;exports.hello=()=>run('hello')" | out-file -encoding ASCII scripts/tasks.js
+echo "if(process.env['MY_VAR']!=='my-var')throw 'not ok' " | out-file -encoding ASCII scripts/tasks/hello.js
 
 npm run hello
 
